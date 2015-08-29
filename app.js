653c483dd2840d124b29bfed4f94831fb82cb81a
luobotang <tangkangxing@sina.com>
2015-08-29 11:08:46 +0800

    movieRenderAndWriter

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ var path = require('path')
 var dateFormat = require('dateformat')
 
 var MovieParser = require('./lib/movieParser')
+var MovieRenderAndWriter = require('./lib/movieRenderAndWriter')
 
 var WEB_SITE_ROOT = 'http://www.dy2018.com/'
 var cacheDirectory = 'tmp'
 var now = new Date()
 var today = dateFormat(now, 'yyyy-mm-dd')
 var currentTime = dateFormat(now, 'yyyy-mm-dd_hh')
+var outputfile = 'out/movie.html'
 
 http.get(WEB_SITE_ROOT, function (res) {
 	var cacheFileStream = fs.createWriteStream(path.resolve(cacheDirectory, currentTime + '.html'))
@@ -19,45 +21,11 @@ http.get(WEB_SITE_ROOT, function (res) {
 		cacheFileStream.write(chunk)
 	})
 	res.on('end', function () {
-		findAndSaveMovies(Buffer.concat(data))
+		// 按单字节处理，避免编码问题
+		var html = Buffer.concat(data).toString('binary')
+		var movies = MovieParser.parse(html)
+		var htmlTitle = 'Movies' + today
+		MovieRenderAndWriter.renderAndSave(movies, outputfile, htmlTitle)
 		cacheFileStream.end()
 	})
 })
-
-function findAndSaveMovies(buffer) {
-	var outFileStream = fs.createWriteStream('out/movie.html', {encoding: 'binary'})
-	outFileStream.write(new Buffer(
-		'<!doctype html>' +
-		'<html lang="en">' +
-		'<head>' +
-		'<meta charset="GBK">' +
-		'<link href="movie.css" rel="stylesheet">' +
-		'<title>Movies - ' + today + '</title>' +
-		'</head>' +
-		'<body>\n' +
-		'<ul>\n',
-		'binary'
-		)
-	)
-
-	var site = WEB_SITE_ROOT.slice(0, -1)
-	
-	MovieParser.parse(buffer.toString('binary')) // 按单字节处理，避免编码问题
-	.forEach(function (movie) {
-		outFileStream.write(new Buffer(
-			'<li>' +
-				'<a href="' + site + movie.url + '" target="_blank">' + movie.name + '</a>' +
-			'</li>\n',
-			'binary'
-			)
-		)
-	})
-
-	outFileStream.end(new Buffer(
-		'</ul>' +
-		'</body>' +
-		'</html>',
-		'binary'
-		)
-	)
-}
