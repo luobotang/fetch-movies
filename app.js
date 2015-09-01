26c6e97be916bfd77dab11faf2f6e1e84e16bc38
luobotang <tangkangxing@sina.com>
2015-09-01 14:21:31 +0800

    exports as module

--- app.js
+++ app.js
@@ -1,34 +1,26 @@
-var http = require('http')
-var fs = require('fs')
 var path = require('path')
 var dateFormat = require('dateformat')
 
-var MovieParser = require('./lib/movieParser')
+var fetchMovies = require('./lib/fetchMovies')
 var MovieRenderAndWriter = require('./lib/movieRenderAndWriter')
 
 var WEB_SITE_ROOT = 'http://www.dy2018.com/'
 var cacheDirectory = 'tmp'
 var now = new Date()
-var today = dateFormat(now, 'yyyy-mm-dd')
-var currentTime = dateFormat(now, 'yyyy-mm-dd_hh')
 var outputfile = 'out/movie.html'
+var timeout = 5000
 
-http.get(WEB_SITE_ROOT, function (res) {
-	var cacheFileStream = fs.createWriteStream(path.resolve(cacheDirectory, currentTime + '.html'))
-	var data = []
-	res.on('data', function (chunk) {
-		data.push(chunk)
-		cacheFileStream.write(chunk)
+fetchMovies({
+	cacheFile: path.resolve(cacheDirectory, dateFormat(now, 'yyyy-mm-dd_hh') + '.html'),
+	timeout: timeout
+}).then(function (movies) {
+	console.log('fetch success, got ' + movies.length + ' movies')
+	console.log('try save to ' + outputfile + ' ...')
+	MovieRenderAndWriter.renderAndSave(movies, {
+		output: outputfile,
+		title: dateFormat(now, 'yyyy-mm-dd') + ' Movies',
+		site: WEB_SITE_ROOT.slice(0, -1)
 	})
-	res.on('end', function () {
-		// 按单字节处理，避免编码问题
-		var html = Buffer.concat(data).toString('binary')
-		var movies = MovieParser.parse(html)
-		MovieRenderAndWriter.renderAndSave(movies, {
-			output: outputfile,
-			title: today + ' Movies',
-			site: WEB_SITE_ROOT.slice(0, -1)
-		})
-		cacheFileStream.end()
-	})
+}, function (err) {
+	console.log(err)
 })
