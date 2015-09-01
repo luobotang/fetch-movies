7f504836a9c2a48adb32fa5df854fa5318c3083b
luobotang <tangkangxing@sina.com>
2015-09-01 14:48:38 +0800

    refactor and support GBK decoding

--- app.js
+++ app.js
@@ -4,22 +4,20 @@ var dateFormat = require('dateformat')
 var fetchMovies = require('./lib/fetchMovies')
 var MovieRenderAndWriter = require('./lib/movieRenderAndWriter')
 
-var WEB_SITE_ROOT = 'http://www.dy2018.com/'
 var cacheDirectory = 'tmp'
 var now = new Date()
 var outputfile = 'out/movie.html'
 var timeout = 5000
 
 fetchMovies({
-	cacheFile: path.resolve(cacheDirectory, dateFormat(now, 'yyyy-mm-dd_hh') + '.html'),
+	cacheFile: path.resolve(cacheDirectory, dateFormat(now, 'yyyy-mm-dd_HH') + '.html'),
 	timeout: timeout
 }).then(function (movies) {
 	console.log('fetch success, got ' + movies.length + ' movies')
 	console.log('try save to ' + outputfile + ' ...')
-	MovieRenderAndWriter.renderAndSave(movies, {
+	MovieRenderAndWriter(movies, {
 		output: outputfile,
 		title: dateFormat(now, 'yyyy-mm-dd') + ' Movies',
-		site: WEB_SITE_ROOT.slice(0, -1)
 	})
 }, function (err) {
 	console.log(err)
