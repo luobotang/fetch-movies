88d73119d9c7e5bd9c6e162e6b4d1dabf6ee7167
luobotang <tangkangxing@sina.com>
2015-09-11 15:13:30 +0800

    simply support client browser

--- server.js
+++ server.js
@@ -6,16 +6,18 @@ var PORT = 8080
 app
 .use('/getAllMovie', getAllMovies)
 .use('/getMovie', getMovie)
-.use(serveStatic('app', {
-	index: ['index.htm']
+.use(serveStatic('out', {
+	index: ['movie.html']
 })).listen(PORT)
 
 function getAllMovies(req, res, next) {
 	// todo
+	next()
 }
 
 function getMovie(req, res, next) {
 	// todo
+	next()
 }
 
 console.log('server started on port: ' + PORT)
